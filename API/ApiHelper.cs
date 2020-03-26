491c6ea2294bb3c5ac967d6b379d018becbad6d8
Nove developer <nove1398@hotmail.com>
2020-03-26 00:57:59 -0500

    Add project files.

---
+++ API/ApiHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace APILib
+{
+    public class ApiHelper
+    {
+        public static HttpClient _client { get; set; } = new HttpClient();
+
+        public ApiHelper()
+        {
+        }
+    }
+}
